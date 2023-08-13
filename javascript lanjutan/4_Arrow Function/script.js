4576de46f3d1763a4b0966a1e5f4150717b59b29
nioke-dev <nioke8090@gmail.com>
2023-08-14 01:02:53 +0700

    This Pada Errow Function

--- javascript lanjutan/4_Arrow Function/script.js
+++ javascript lanjutan/4_Arrow Function/script.js
@@ -4,7 +4,6 @@
 // }
 // console.log(tampilNama('sandhika'));
 
-
 // bentuk 1 arrow function
 
 // const tampilNama = (nama) => {
@@ -39,7 +38,73 @@
 
 // jika kita hanya menulikan kurung kurawal biasa maka akan dianggap return
 // untuk menjadikan kurung kurawal tersebut sebagai objek tambahkan kurung biasa
-// let jumlahHuruf = mahasiswa.map(nama => ({ 
+// let jumlahHuruf = mahasiswa.map(nama => ({
 //     name: nama,
 //     jmlHuruf: nama.length
 // }));
+
+// Konsep this pada arrow function
+// Constructor Function
+
+// const Mahasiswa = function () {
+//     this.nama = 'Sandhika',
+//     this.umur = 33,
+//     console.log(this);
+//     this.sayHello = function () {
+//         console.log(`Halo Nama Saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     }
+// }
+
+// const sandhika = new Mahasiswa();
+
+// Arrow Function
+// const Mahasiswa = function () {
+//     this.nama = 'Sandhika',
+//     this.umur = 33,
+//     console.log(this);
+//     this.sayHello = () => {
+//         console.log(`Halo Nama Saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     }
+// }
+
+// const sandhika = new Mahasiswa();
+
+// Object Literal
+// const mhs1 = {
+//   nama: "sandhika",
+//   umur: 33,
+//   sayHello: () => {
+//     console.log(`Halo Nama Saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     console.log(this);
+//   },
+// };
+
+
+// const Mahasiswa = function () {
+//     this.nama = 'sandhika',
+//     this.umur = 33,
+//     this.sayHello = function () {
+//         console.log(`Halo nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     }
+//     setInterval(() => {
+//         console.log(this.umur++);
+//     }, 500);
+// }
+
+// const sandhika = new Mahasiswa(); 
+
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function () {  
+    let satu = 'size';
+    let dua = 'caption';
+    if (this.classList.contains(satu)) {
+        [satu, dua] = [dua, satu];
+    }  
+    this.classList.toggle(satu);
+    // toggle artinya akan menambahkan class baru jika belum ada
+    // atau dia akan menghilangkan class jika ada
+    setTimeout(() => {
+        this.classList.toggle(dua);
+    }, 600);
+});
